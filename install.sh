42703a2256d2b57589bb13dc42af8925cbd2bb1a
David Nuon <david@davidnuon.com>
2015-01-31 15:21:53 -0800

    Changes

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 sudo apt-get update && sudo apt-get -y upgrade
-sudo apt-get -y install python-software-properties
-sudo add-apt-repository -y ppa:chris-lea/node.js
+sudo apt-get -y install python-software-properties curl build-essential
+curl -sL https://deb.nodesource.com/setup | sudo bash -
 sudo apt-get -y install nodejs
-sudo apt-get -y install npm
-sudo apt-key -y adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+sudo apt-key  adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
-sudo apt-get update
-sudo npm install -g generator-keystone
+sudo apt-get update && sudo apt-get install -y mongodb-org
+npm install generator-keystone
