096f9e16017189d6d00808d245bfcb1d277af475
Shane Satterfield <dustyplant@gmail.com>
2015-01-31 14:22:23 -0800

    Now should install properly.

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 sudo apt-get update && sudo apt-get -y upgrade
-sudo apt-get install python-software-properties
-sudo add-apt-repository ppa:chris-lea/node.js
-sudo apt-get install nodejs
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+sudo apt-get -y install python-software-properties
+sudo add-apt-repository -y ppa:chris-lea/node.js
+sudo apt-get -y install nodejs
+sudo apt-key -y adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt-get update
 sudo npm install -g generator-keystone
