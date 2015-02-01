4c249a7ca5f1a4b7c2da03613cf7ebe1e10af99a
David Nuon <david@davidnuon.com>
2015-01-31 16:02:28 -0800

    Able to install keystonejs

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 sudo apt-get update && sudo apt-get -y upgrade
 sudo apt-get -y install python-software-properties curl build-essential
-curl -sL https://deb.nodesource.com/setup | sudo bash -
+sudo curl -sL https://deb.nodesource.com/setup | sudo bash -
 sudo apt-get -y install nodejs
 sudo apt-key  adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt-get update && sudo apt-get install -y mongodb-org
-npm install generator-keystone
+mkdir ~/npm
+npm config set prefix ~/npm
+export PATH="$PATH:$HOME/npm/bin"
+echo export PATH="$PATH:$HOME/npm/bin" >> ~/.bashrc && source ~/.bashrc
+npm install -g generator-keystone
